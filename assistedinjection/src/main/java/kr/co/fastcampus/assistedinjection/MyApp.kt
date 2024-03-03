0dc34342c1821b7fbf67b854c12b8e4a3c382e8f
soohwan.ok <soohwan.ok@navercorp.com>
2024-03-03 17:19:35 +0900

    [Part4][Ch02][Clip05] CustomInjection

--- assistedinjection/src/main/java/kr/co/fastcampus/assistedinjection/MyApp.kt
+++ assistedinjection/src/main/java/kr/co/fastcampus/assistedinjection/MyApp.kt
@@ -1,11 +1,30 @@
 package kr.co.fastcampus.assistedinjection
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.migration.CustomInject
+import dagger.hilt.android.migration.CustomInjection
+import javax.inject.Inject
 
 /**
  * @author soohwan.ok
  */
+
+class Foo @Inject constructor()
+
 @HiltAndroidApp
+@CustomInject
 class MyApp : Application() {
+
+    @Inject
+    lateinit var foo:Foo
+
+    override fun onCreate() {
+        Log.e("MyApp", "foo1 = ${this::foo.isInitialized}")
+        super.onCreate()
+        Log.e("MyApp", "foo2 = ${this::foo.isInitialized}")
+        CustomInjection.inject(this)
+        Log.e("MyApp", "foo3 = ${this::foo.isInitialized}")
+    }
 }
